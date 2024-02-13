c1b59569e2570157925332352efa696c8a90a052
SzKriszti <krisztinaszabo98@gmail.com>
2024-02-13 13:06:53 +0100

    scss design

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-const root = document.querySelector("#root")
+const root = document.querySelector('#root')
 
-const header = root.insertAdjacentHTML("beforeend", `
- <header>New York Times Bestsellers</header>
+const skeleton = () => `
+  <header class='header'>The New York Times Best Sellers</header>
+  <div class='books'></div>
+`
+
+const bookComponent = (book) => `
+  <div class='book'>
+    <h2>${book.title}</h2>
+    <h4>${book.sub}</h4>
+    <p>${book.text}</p>
+    <div class='number'>${book.id}</div>
+    <button>read more</button>
+  </div>
+`
+
+const loadEvent = () => {
+  root.innerHTML = skeleton()
+
+  const booksElement = document.querySelector('.books')
+  
+  books.forEach(book => {
+    booksElement.insertAdjacentHTML('beforeend', bookComponent(book))
+  })
+}
+
+window.addEventListener('load', loadEvent)
+
+
+
+
+
+/*const header = root.insertAdjacentHTML("beforeend", `
+ <header>The New York Times Best Sellers</header>
 `)
+
 
 books.forEach((book, index) => root.insertAdjacentHTML("beforeend", `
   <div class="book">
 
-    <h4>${book.sub}</h4>
     <h2>${book.title}</h2>
+    <h4>${book.sub}</h4>
     <p>${book.text}</p>
 
     <div class="number">${index+1}</div>
@@ -16,4 +48,6 @@ books.forEach((book, index) => root.insertAdjacentHTML("beforeend", `
     <button>read more</button>
 
   </div>
-`))
+`))*/
+  
+
